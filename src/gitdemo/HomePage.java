3341f6fde11f76a4241ba066b9e3cf62fc4d4932
STPLearners <124543452+STPLearners@users.noreply.github.com>
2023-02-07 16:20:45 +0530

    Update HomePage.java
    Sysout added from GitHub

--- src/gitdemo/HomePage.java
+++ src/gitdemo/HomePage.java
@@ -3,6 +3,7 @@ package gitdemo;
 public class HomePage {
 
 	public static void main(String[] args) {
+		System.out.println("This line of statement is updated from GitHub at 07-02 16:19");
 		
 	}
 
